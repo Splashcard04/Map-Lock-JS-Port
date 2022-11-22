fb33c7f0caa69052586923264191e29fbb2c8522
TsarK0480 <anthony_locascio@icloud.com>
2022-11-22 00:50:27 -0800

    Added this, I hope it works.

---
+++ wallsbetween.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @param {*} t1 Time it starts
+ * @param {*} t2 Time it ends
+ * @param {*} customWallData The custom data, for now only 1 customdata for between v1
+ */
+
+
+
+function wallsBetween(t1, t2, customWallData){
+    filteredwalls = walls.filter(n => n._time >= t1 && n._time < t1);
+    filteredwalls.forEach(wall => {
+        wall._customData._animation = {}
+        customWallData
+    })
+}
+
+export default wallsBetween;
+
+
