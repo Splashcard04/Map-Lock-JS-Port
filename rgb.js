b8bb7d9066d95eed75f259eef67866372c60f25c
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-21 22:05:53 -0800

    Added these as well

---
+++ rgb.js
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * @param {*} val1 input any RGB value (NOT DECIMAL) and it'll format
+ * @returns yea
+ */
+
+
+function ezRGB(val1){
+    return val1 / 255
+}
+
+export default ezRGB;
